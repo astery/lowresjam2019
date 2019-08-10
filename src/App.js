0bf733d778d0463d30eb481f3175c5cc285388ac
Andrey Akulov <andreyshark@gmail.com>
2019-08-10 15:45:05 +0300

    Add basic movement

--- src/App.js
+++ src/App.js
@@ -1,34 +1,138 @@
-import React, { useState, useCallback } from 'react';
-import './App.css';
-
-import { Stage, Layer, Rect, Text } from 'react-konva';
+import React, { useState, useEffect, useCallback, useRef } from 'react';
+import { Stage, Layer, Rect } from 'react-konva';
 import Konva from 'konva';
 
-function ColoredRect() {
-  const [color, setColor] = useState(Konva.Util.getRandomColor())
-  const setRandomColor = useCallback(() => {
-    setColor(Konva.Util.getRandomColor())
-  }, [])
+const frameDelay = 1000 / 60;
+const scale = 10;
+const width = 64;
+const realWidth = scale * width;
+const bgColor = '#5D1D45';
+
+function useInterval(callback, delay) {
+  const savedCallback = useRef();
+
+  // Remember the latest callback.
+  useEffect(() => {
+    savedCallback.current = callback;
+  }, [callback]);
+
+  // Set up the interval.
+  useEffect(() => {
+    function tick() {
+      savedCallback.current();
+    }
+    if (delay !== null) {
+      let id = setInterval(tick, delay);
+      return () => clearInterval(id);
+    }
+  }, [delay]);
+}
+
+class Vector {
+  constructor(x, y) {
+    this.x = x
+    this.y = y
+  }
+
+  add(v) {
+    return new Vector(this.x + v.x, this.y + v.y)
+  }
+}
+
+function Hero({position, color}) {
+  const w = width/6
+  const x = position.x - w/2
+  const y = position.y - w/2
 
   return (
     <Rect
-      x={20}
-      y={20}
-      width={50}
-      height={50}
+      x={x}
+      y={y}
+      width={w}
+      height={w}
       fill={color}
-      shadowBlur={5}
-      onClick={setRandomColor}
+    />
+  );
+}
+
+function Background() {
+  return (
+    <Rect
+      x={0}
+      y={0}
+      width={width}
+      height={width}
+      fill={bgColor}
     />
   );
 }
 
 function App() {
+  const [tick, setTick] = useState(0)
+  const [heroPosition, setHeroPosition] = useState(new Vector(width/2, width/2))
+  const [heroMove, setHeroMove] = useState(new Vector(0, 0))
+  const [heroColor, _] = useState(Konva.Util.getRandomColor())
+
+  const onKey = useCallback((e) => {
+    const keyName = e.key;
+    const force = 1;
+    console.log(e.type, keyName)
+
+    if (e.type === 'keydown') {
+      switch (keyName) {
+        case 'ArrowUp':
+          setHeroMove(new Vector(0, -force))
+          return;
+        case 'ArrowDown':
+          setHeroMove(new Vector(0, force))
+          return;
+        case 'ArrowLeft':
+          setHeroMove(new Vector(-force, 0))
+          return;
+        case 'ArrowRight':
+          setHeroMove(new Vector(force, 0))
+          return;
+        default:
+          return;
+      }
+    } else if (e.type === 'keyup') {
+      switch (keyName) {
+        case 'ArrowUp':
+          setHeroMove(new Vector(0, 0))
+          return;
+        case 'ArrowDown':
+          setHeroMove(new Vector(0, 0))
+          return;
+        case 'ArrowLeft':
+          setHeroMove(new Vector(0, 0))
+          return;
+        case 'ArrowRight':
+          setHeroMove(new Vector(0, 0))
+          return;
+        default:
+          return;
+      }
+    }
+  }, [setHeroMove]);
+
+  useEffect(() => {
+    window.addEventListener('keydown', onKey);
+    window.addEventListener('keyup', onKey);
+  }, [])
+
+  const updateTick = useCallback(() => setTick(tick + 1), [tick])
+
+  useInterval(updateTick, frameDelay)
+
+  useEffect(() => {
+    setHeroPosition(heroPosition.add(heroMove))
+  }, [tick]);
+
   return (
-    <Stage width={window.innerWidth} height={window.innerHeight}>
+    <Stage width={realWidth} height={realWidth} scaleX={scale} scaleY={scale}>
       <Layer>
-        <Text text="Try click on rect" />
-        <ColoredRect />
+        <Background />
+        <Hero position={heroPosition} color={heroColor} />
       </Layer>
     </Stage>
   );
